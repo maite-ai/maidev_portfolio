50f9dad590cd68a7c4f266d7b42724aae23ec3b9
M. Fernanda Rios <64336818+maite-ai@users.noreply.github.com>
2025-11-17 14:29:44 -0300

    fix(build_files): add a virtaulenv creation

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+echo "🚀 Initializing Mai portfolio - Python Developer"
+echo "=================================================="
+echo ""
+
+# Check if virtual environment exists
+VIRTUALENV="portfolio"
+if [ -d "$HOME/.pyenv/versions/$VIRTUALENV" ]; then
+  echo "The '$VIRTUALENV' virtualenv exists"
+else
+  echo "📦 Creating virtual environment..."
+  pyenv virtualenv 3.12.9 mai-portfolio
+fi
+
+# Activate virtual environment
+echo "⚡ Activating virtual environment..."
+pyenv local portfolio
+
 # Build script for Vercel deployment
 echo "Building Django project for Vercel..."
 
 # Install dependencies
+echo "📥 Installing dependencies..."
 pip install -r requirements.txt
 
+# Run migrations
+#echo "🗃️ Executing migrations..."
+#python manage.py migrate
+
 # Collect static files
+echo "📁 Collecting statics files..."
 python manage.py collectstatic --noinput --clear
 
 echo "📁 Preparing static files for Vercel..."
